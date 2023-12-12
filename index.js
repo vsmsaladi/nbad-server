8f9a549cfbe0d646032ddff1a6a887701e89ca5e
Santosh <vsaladi@uncc.edu>
2023-12-12 02:29:39 -0500

    to setup gzip compression

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const routes = require("./routes");
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
+const compression = require('compression');
 
+app.use(compression());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
